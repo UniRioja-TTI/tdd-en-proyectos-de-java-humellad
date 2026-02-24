2307fcd1cf8295d6254fc17e541ecd4caf114aa2
humellad <144775542+humellad@users.noreply.github.com>
2026-02-24 15:42:10 +0100

    Create DBStub.java

---
+++ src/main/java/com/tt1/test/DBStub.java
@@ -0,0 +1,20 @@
+public class DBStub{
+  private List<ToDo> BD
+  private List<string> emails
+
+  public void CREATE(ToDo toDo){
+  }
+
+  public ToDo READ(ToDo toDo){
+
+  }
+
+  public void UPDATE(ToDo toDo){
+
+  }
+
+  public void DELETE(ToDo toDo){
+
+  }
+
+}
