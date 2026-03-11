d88540babd417c8401cd62e169b0f49fe4abbcae
Hugo <humellad@unirioja.es>
2026-03-11 12:07:49 +0100

    Fin

--- src/test/java/mocks/RepositorioMock.java
+++ src/test/java/mocks/RepositorioMock.java
@@ -6,54 +6,62 @@ import Interfaces.IRepositorio;
 
 public class RepositorioMock implements IRepositorio {
     public boolean guardarLlamado = false;
+    private List<ToDo> tareas = new ArrayList<>();
     private List<String> emailsSimulados = new ArrayList<>();
 
     @Override
     public void guardarNuevoToDo(ToDo todo) {
         this.guardarLlamado = true;
+        tareas.add(todo);
     }
 
     
     public boolean contieneEmail(String email) {
         return emailsSimulados.contains(email);
     }
-    
-    public void annadirEmailManual(String email) {
-        emailsSimulados.add(email);
-    }
-
 
 	@Override
 	public ToDo buscarPorNombre(String nombre) {
-		// TODO Auto-generated method stub
-		return null;
+    	for(ToDo sol : this.tareas) {
+    		if(sol.getNombre().equals(nombre)) {
+    			return sol;
+    		}
+    	}
+    	return null;
 	}
 
 
 	@Override
 	public void marcarComoCompletada(String nombre) {
-		// TODO Auto-generated method stub
-		
+		for(ToDo sol : this.tareas) {
+    		if(sol.getNombre().equals(nombre)) {
+    			sol.setCompletado(true);
+    		}
+    	}
 	}
 
 
 	@Override
 	public List<ToDo> obtenerPendientes() {
-		// TODO Auto-generated method stub
-		return null;
+    	List<ToDo> res = new ArrayList<ToDo>();
+    	for(ToDo aux : this.tareas) {
+    		if(!aux.completado()) {
+    			res.add(aux);
+    		}
+    	}
+        return res;
 	}
 
 
 	@Override
 	public void guardarEmail(String email) {
-		// TODO Auto-generated method stub
-		
+		emailsSimulados.add(email);
 	}
 
 
 	@Override
 	public List<String> obtenerTodosLosEmails() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> sol = new ArrayList<String>(this.emailsSimulados)
+		return sol;
 	}
 }
